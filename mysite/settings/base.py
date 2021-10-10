722d380d7e49b32a7273d5d03bb3e4093147569c
Yariv Friedman <yrfriedman@gmail.com>
2021-10-10 17:55:48 +0200

    initial commit

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -106,14 +106,23 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dbcar',
+        'USER': 'caruser',
+        'PASSWORD': '2020',
+        'HOST': '',
+        'PORT': '5432',
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
