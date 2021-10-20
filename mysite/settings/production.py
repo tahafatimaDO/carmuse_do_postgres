374ba4e072de60e5eea0af22ccaee97c506b2301
Dbonbon <yrfriedman@gmail.com>
2021-10-20 08:13:13 +0000

    allowed hosts

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -4,8 +4,8 @@ from django.core.management.utils import get_random_secret_key
 DEBUG = int(os.environ.get("DEBUG", default=1))
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 
-#ALLOWED_HOSTS = ["DJANGO_ALLOWED_HOSTS", "127.0.0.1"]
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1").split(",")
+ALLOWED_HOSTS = ["DJANGO_ALLOWED_HOSTS", "127.0.0.1"]
+# ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1").split(",")
 
 # try:
 #     from .local import *
