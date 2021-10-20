8f7173bcd46cabc53d798d29a3190a885452f027
Dbonbon <yrfriedman@gmail.com>
2021-10-20 07:52:59 +0000

    move local host from allowed hosts

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -5,7 +5,7 @@ DEBUG = int(os.environ.get("DEBUG", default=1))
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 
 #ALLOWED_HOSTS = ["DJANGO_ALLOWED_HOSTS", "127.0.0.1"]
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1").split(",")
 
 # try:
 #     from .local import *
