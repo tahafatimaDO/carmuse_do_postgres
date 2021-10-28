93b1a6f3856847535a2201ffdafffd19f37a454d
Dbonbon <yrfriedman@gmail.com>
2021-10-28 14:39:05 +0000

    logo test work on local

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -107,7 +107,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'mysite.wsgi.application'
-
+ 
 DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
 
 if DEVELOPMENT_MODE is True:
@@ -122,8 +122,8 @@ elif len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
         raise Exception("DATABASE_URL environment variable not defined")
     DATABASES = {
         "default": dj_database_url.parse(os.environ.get("DATABASE_URL")),
-    }
-   
+    } 
+  
 #if os.environ.get('DATABASE_URL'):
 #    DATABASES['default'] =dj_database_url.config(default=os.environ['DATABASE_URL'])
 
