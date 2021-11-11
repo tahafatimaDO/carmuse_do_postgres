ce4b0323f0e26622d229b9f2bf7f1b2a85c6adf1
tahafatimaDO <82136225+tahafatimaDO@users.noreply.github.com>
2021-11-11 21:50:59 +0530

    Update dev.py

--- mysite/settings/dev.py
+++ mysite/settings/dev.py
@@ -58,7 +58,7 @@ AWS_S3_OBJECT_PARAMETERS = {
 AWS_LOCATION = 'taha_new'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'mysite/static'),
+    os.path.join(BASE_DIR, 'static'),
 ]
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_ENDPOINT_URL, AWS_LOCATION)
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
